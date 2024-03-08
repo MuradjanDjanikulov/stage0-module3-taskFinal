108c19a6e360d4009a53a1e75165da0e18e66a6d
murod <murad.djanikulov@gmail.com>
2024-03-08 23:47:51 +0500

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String alfa = "";
+        alfa += number%10;
+        number = number/10;
+        alfa+=number%10;
+        number = number/10;
+        alfa+=number%10;
+        System.out.println(alfa);
+
     }
 }
